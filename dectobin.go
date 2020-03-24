7d5af7577ca5b938fb001cf2c1e96717ddd4e340
Chukwu Austin <35623912+Austin-Deccentric@users.noreply.github.com>
2020-03-24 22:50:41 +0100

    Update dectobin.go

--- dectobin.go
+++ dectobin.go
@@ -4,23 +4,22 @@ import(
 	"fmt"
 	"strconv"
 )
-
+// Conv converts the user input to equivalnet binary representation
 func conv(n int) string{
-	return strconv.FormatInt(int64 (n), 2)
+	return strconv.FormatInt(int64 (n), 2) 
 }
 
 func main() {
 	var s string
-	//var i int
 	fmt.Println("Enter a postive Number:")
 	for{
-		_, err := fmt.Scan(&s)
-		i, err := strconv.Atoi(s)
-		if err != nil {
+		_, err := fmt.Scan(&s) // Read from standard input using Scan and a pointer. _, err because i want to capture any error
+		i, err := strconv.Atoi(s) // Converts the input to an interger
+		if err != nil {           // Checks for err in the conversion and repeats the stdio operation
 			fmt.Println("Enter a valid number")
 		} else{
-			fmt.Println(conv(i))
-			break
+			fmt.Println(conv(i)) //If no error is encountered, call on the conversion function: conv
+			break		     // exit the loop
 		}
 	}
 }
