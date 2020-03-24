a742d48c23edf3da8038ecf8aa236802eda02c90
Chukwu Austin <35623912+Austin-Deccentric@users.noreply.github.com>
2020-03-24 22:53:10 +0100

    Update dectobin.go

--- dectobin.go
+++ dectobin.go
@@ -9,17 +9,17 @@ func conv(n int) string{
 	return strconv.FormatInt(int64 (n), 2) 
 }
 
-func main() {
-	var s string
+func main() {				
+	var s string 				//Initialize a variable of type string
 	fmt.Println("Enter a postive Number:")
 	for{
-		_, err := fmt.Scan(&s) // Read from standard input using Scan and a pointer. _, err because i want to capture any error
-		i, err := strconv.Atoi(s) // Converts the input to an interger
-		if err != nil {           // Checks for err in the conversion and repeats the stdio operation
+		_, err := fmt.Scan(&s) 		//Read from standard input using Scan and a pointer. _, err because i want to capture any error
+		i, err := strconv.Atoi(s) 	// Converts the input to an interger
+		if err != nil {           	// Checks for err in the conversion and repeats the stdio operation
 			fmt.Println("Enter a valid number")
 		} else{
-			fmt.Println(conv(i)) //If no error is encountered, call on the conversion function: conv
-			break		     // exit the loop
+			fmt.Println(conv(i)) 	//If no error is encountered, call on the conversion function: conv
+			break		     	// exit the loop
 		}
 	}
 }
